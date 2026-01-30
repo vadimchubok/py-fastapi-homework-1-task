81e3cf5fe04ab579200434d96649914e1efcf46d
Vadim <vadimchubok@gmail.com>
2026-01-30 09:34:27 +0200

    fix

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -10,26 +10,31 @@ from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 router = APIRouter()
 
 
-@router.get("/movies", response_model=MovieListResponseSchema)
+@router.get("/movies/", response_model=MovieListResponseSchema)
 async def get_movies(
         page: int = Query(1, ge=1),
-        limit: int = Query(10, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
         db: AsyncSession = Depends(get_db)
 ):
-    offset = (page - 1) * limit
+    offset = (page - 1) * per_page
 
     count_query = select(func.count()).select_from(MovieModel)
     count_result = await db.execute(count_query)
-    total_items = count_result.scalar()
+    total_items = count_result.scalar() or 0
 
-    total_pages = math.ceil(total_items / limit) if total_items > 0 else 1
+    if total_items == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
 
-    movie_query = select(MovieModel).offset(offset).limit(limit)
+    total_pages = math.ceil(total_items / per_page)
+
+    movie_query = select(MovieModel).offset(offset).limit(per_page)
     movie_result = await db.execute(movie_query)
     movies = movie_result.scalars().all()
 
-    prev_page = f"/movies?page={page - 1}&limit={limit}" if page > 1 else None
-    next_page = f"/movies?page={page + 1}&limit={limit}" if page < total_pages else None
+    prev_page = (f"/movies/?page={page - 1}"
+                 f"&per_page={per_page}")if page > 1 else None
+    next_page = (f"/movies/?page={page + 1}"
+                 f"&per_page={per_page}") if page < total_pages else None
 
     return {
         "movies": movies,
@@ -40,13 +45,16 @@ async def get_movies(
     }
 
 
-@router.get("/movies/{movie_id}", response_model=MovieDetailResponseSchema)
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
 async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
     query = select(MovieModel).where(MovieModel.id == movie_id)
     result = await db.execute(query)
     movie = result.scalar_one_or_none()
 
     if not movie:
-        raise HTTPException(status_code=404, detail="Movie not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
 
     return movie
