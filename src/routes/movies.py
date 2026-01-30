06bcdcbf8ec3cc618a6ed9caf87a8ba79d0c99e6
Vadim <vadimchubok@gmail.com>
2026-01-30 09:38:21 +0200

    fix

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -22,17 +22,20 @@ async def get_movies(
     count_result = await db.execute(count_query)
     total_items = count_result.scalar() or 0
 
-    if total_items == 0:
-        raise HTTPException(status_code=404, detail="No movies found.")
+    total_pages = math.ceil(total_items / per_page) if total_items > 0 else 0
 
-    total_pages = math.ceil(total_items / per_page)
+    if total_items == 0 or page > total_pages:
+        raise HTTPException(status_code=404, detail="No movies found.")
 
     movie_query = select(MovieModel).offset(offset).limit(per_page)
     movie_result = await db.execute(movie_query)
     movies = movie_result.scalars().all()
 
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
     prev_page = (f"/movies/?page={page - 1}"
-                 f"&per_page={per_page}")if page > 1 else None
+                 f"&per_page={per_page}") if page > 1 else None
     next_page = (f"/movies/?page={page + 1}"
                  f"&per_page={per_page}") if page < total_pages else None
 
